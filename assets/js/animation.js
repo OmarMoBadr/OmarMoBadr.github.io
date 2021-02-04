80549babd873c48ded280a98ec0fb6faeb9f7479
OmarMoBadr <omarmobadr@gmail.com>
2021-02-04 19:54:04 +0200

    some visual updates

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -6,11 +6,11 @@ tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 
 gsap.from('.home_img', {opacity: 0, duration: 2, delay: 3, x: 60})
 
-gsap.from('.home_information', {opacity: 0, duration: 1.5, delay: 3.3, y: 25})
-gsap.from('.anime-text', {opacity: 0, duration: 1.5, delay: 3.3, y: 25, ease:'expo.out', stagger: .3})
+gsap.from('.home_information', {opacity: 0, duration: 1, delay: 3.3, y: 25})
+gsap.from('.anime-text', {opacity: 0, duration: 1, delay: 3.3, y: 25, ease:'expo.out', stagger: .3})
 
-gsap.from('.nav_logo', {opacity:0, duration: 1.5, delay: 4.2, y: 25, ease:'expo.out'});
-gsap.from('.nav_toggle', {opacity:0, duration: 1.5, delay: 4.2, y: 25, ease:'expo.out'});
-gsap.from('.nav_item', {opacity: 0, duration: 1.5, delay: 4.2, y: 25, ease:'expo.out', stagger: .2})
+gsap.from('.nav_logo', {opacity:0, duration: 1, delay: 3.8, y: 25, ease:'expo.out'});
+gsap.from('.nav_toggle', {opacity:0, duration: 1, delay: 3.8, y: 25, ease:'expo.out'});
+gsap.from('.nav_item', {opacity: 0, duration: 1, delay: 3.8, y: 25, ease:'expo.out', stagger: .2})
 
-gsap.from('.home_social-icon', {opacity: 0, duration: 1.5, delay: 5, y: 25, ease:'expo.out', stagger: .2})
+gsap.from('.home_social-icon', {opacity: 0, duration: 1, delay: 4.4, y: 25, ease:'expo.out', stagger: .2})
